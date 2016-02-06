8ae7652cc4c1c5162687d28f18293364d67446ff
Gabriel Massana <gabrielmassana@gmail.com>
2016-02-06 00:04:09 +0000

    added missing classes

---
+++ Cocoapods/ViewControllers/Pod/CCPPodsViewController.m
@@ -0,0 +1,22 @@
+//
+//  CCPPodsViewController.m
+//  Cocoapods
+//
+//  Created by Gabriel Massana on 5/2/16.
+//  Copyright © 2016 Gabriel Massana. All rights reserved.
+//
+
+#import "CCPPodsViewController.h"
+
+@implementation CCPPodsViewController
+
+#pragma mark - ViewLifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    self.view.backgroundColor = [UIColor whiteColor];
+}
+
+@end
